938674c6dd51b843a685c06d46469d43f931245d
bjorndecraemer <bjorn.decraemer@yahoo.com>
2019-01-07 16:23:56 +0100

    Initial commit

---
+++ src/main/java/bjorn/petprojects/bpppetbook/BppPetBookApplication.java
@@ -0,0 +1,14 @@
+package bjorn.petprojects.bpppetbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BppPetBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BppPetBookApplication.class, args);
+    }
+
+}
+
