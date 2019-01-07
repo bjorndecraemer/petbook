8cf30b6a5562e568ac6459fdb470515eb823a086
bjorndecraemer <bjorn.decraemer@yahoo.com>
2019-01-07 19:57:09 +0100

    Added Test for HomeController and removed old JUnit4 issues

--- pet-book-web/src/test/java/bjorn/petprojects/bpppetbook/controllers/HomeControllerTest.java
+++ pet-book-web/src/test/java/bjorn/petprojects/bpppetbook/controllers/HomeControllerTest.java
@@ -1,17 +1,42 @@
 package bjorn.petprojects.bpppetbook.controllers;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
 public class HomeControllerTest {
 
-    @Before
-    public void setUp() throws Exception {
+
+    @InjectMocks
+    HomeController homeController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
     }
 
     @Test
-    public void getHome() {
+    void getHome() throws Exception{
+        mockMvc.perform(get(""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+        mockMvc.perform(get("/index.html"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 }
